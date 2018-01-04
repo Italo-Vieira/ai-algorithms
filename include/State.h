46d77dadb38a75d5135c2bff689bfbc8ed5799bb
italo <it-jorge@hotmail.com>
2018-01-04 16:21:00 -0200

    Class State, is done

--- include/State.h
+++ include/State.h
@@ -1,6 +1,7 @@
 #ifndef STATE_H
 #define STATE_H
-
+#include <list>
+using std::list;
 template <class DataType>
 class State
 {
@@ -8,10 +9,16 @@ class State
         State();
         virtual ~State();
         virtual bool GenerateChildrens()=0;
+        virtual float getHeuristcValue()=0;
+        virtual float getStateWeight()=0;
+        virtual bool isObjective()=0;
     protected:
 
     private:
-        DataType    data;
+        DataType        data;
+        list<DataType>  childrens;
+        int             childrensCount;
+
 };
 
 #endif // STATE_H
