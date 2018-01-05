9513d88018c48fbf521a76b5b4de6d109a55a186
italo <it-jorge@hotmail.com>
2018-01-05 19:38:38 -0200

    removeEquals now have multithread support (openmp)

--- src/SearchAlgorithm.cpp
+++ src/SearchAlgorithm.cpp
@@ -1,10 +1,16 @@
 #include "SearchAlgorithm.h"
 
 void SearchAlgorithm::removeEquals(list<State*> &from, list<State*> &in){
+  #pragma omp parallel
+  #pragma omp master
   for(iterator itf = from.begin(); itf != from.end(); itf++){
+    #pragma omp task
+    #pragma omp parallel
+    #pragma omp single nowait
     for(iterator iti = in.begin(); iti != in.end(); iti++){
       if((*itf)->equals(*iti)){
         delete *itf;
+        #pragma omp critical(removeFromList)
         itf = from.erase(itf);
         break;
       }
@@ -58,10 +64,9 @@ Path* SearchAlgorithm::DFS(State *state){
   while(!stateStack.empty()){
     State *currentState =  stateStack.front();
     stateStack.pop_front();
-
     if(currentState->isObjective()){
       Path *path = new Path();
-
+      std::cout<<"\nfound\n";
       while(currentState != 0){
         path->addNode(currentState);
         currentState = currentState->father;
@@ -74,13 +79,15 @@ Path* SearchAlgorithm::DFS(State *state){
 
     currentState->GenerateChildrens();
     childrens = currentState->childrens;
+
     removeEquals(childrens, stateStack);
     removeEquals(childrens, closedStates);
-    stateStack.splice(stateStack.begin(), childrens);
+    iterator a = stateStack.begin();
+    stateStack.splice(a, childrens);
     closedStates.push_back(currentState);
 
   }
-  
+
   freeList(closedStates);
   return 0;
 }
