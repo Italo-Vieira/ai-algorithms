bd55b351e78ffdb0623c8350ee13d8275c5a4190
italo <it-jorge@hotmail.com>
2018-01-07 14:06:14 -0200

    .gitignore added

---
+++ include/SearchAlgorithms.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "State.h"
+#include "Path.h"
+
+typedef list<State*>::iterator iterator;
+class SearchAlgorithms{
+  public:
+    static Path* BFS(State*);
+    static Path* DFS(State*);
+  private:
+/*
+Remove from 'from' any state that is also present in 'in'
+*/
+    static void removeEquals(list<State*> &from, list<State*> &in);
+    static void freeList(list<State*>&);
+};
