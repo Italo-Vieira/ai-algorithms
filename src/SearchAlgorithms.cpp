c5749bfd2a77bdef4598687aaac86cf632b21fc0
italo <it-jorge@hotmail.com>
2018-01-08 02:23:19 -0200

    fixed thread bug on removeEquals

--- src/SearchAlgorithms.cpp
+++ src/SearchAlgorithms.cpp
@@ -3,18 +3,21 @@
 void SearchAlgorithms::removeEquals(list<State*> &from, list<State*> &in){
   #pragma omp parallel
   #pragma omp master
-  for(iterator itf = from.begin(); itf != from.end(); itf++){
+  for(iterator itf = from.begin(); itf != from.end();){
+    iterator aux = itf++;
     #pragma omp task
-    #pragma omp parallel
+    #pragma omp parallel private(itf)
     #pragma omp single nowait
     for(iterator iti = in.begin(); iti != in.end(); iti++){
-      if((*itf)->equals(*iti)){
-        delete *itf;
+
+      if((*aux)->equals(*iti)){
+        delete *aux;
         #pragma omp critical(removeFromList)
-        itf = from.erase(itf);
-        break;
+        aux = --from.erase(aux);
+        iti = --in.end();
       }
     }
+
   }
 }
 
@@ -31,20 +34,21 @@ Path* SearchAlgorithms::BFS(State *state){
   list<State*>  childrens;
   stateQueue.push_back(state);
   Path *path;
+
   while(!stateQueue.empty()){
 
     #ifdef _OPENMP
-    int taskNum;
-    #pragma omp parallel
-    #pragma omp single
-    {
-      int queueSize = stateQueue.size();
-      int num_threads = omp_get_team_size(omp_get_level());
-      taskNum = (queueSize > num_threads) ? num_threads : queueSize;
-    }
+      int taskNum;
+      #pragma omp parallel
+      #pragma omp single
+      {
+        int queueSize = stateQueue.size();
+        int num_threads = omp_get_team_size(omp_get_level());
+        taskNum = (queueSize > num_threads) ? num_threads : queueSize;
+      }
 
-    #pragma omp for ordered
-    for (int i = 0; i < taskNum; i++ ){
+      #pragma omp for ordered
+      for (int i = 0; i < taskNum; i++ ){
     #endif
 
     State *currentState ;
@@ -56,8 +60,9 @@ Path* SearchAlgorithms::BFS(State *state){
 
     #pragma single
     if(currentState->isObjective()){
-      std::cout<<"\nyoo\n";
+      //found solution
       Path *path = new Path();
+      //create a path from found soluction
       while(currentState != 0){
         path->addNode(currentState);
         currentState = currentState->father;
@@ -91,15 +96,16 @@ Path* SearchAlgorithms::DFS(State *state){
   list<State*>  childrens;
   stateStack.push_back(state);
   while(!stateStack.empty()){
-    {
-    State *currentState =  stateStack.front();
-    stateStack.pop_front();
-    closedStates.push_back(currentState);
+    std::cout<<"entrou\n";
+    iterator currIt = stateStack.begin();
+    State *currentState =  *currIt;
+
     if(currentState->isObjective()){
       Path *path = new Path();
       std::cout<<"\nfound\n";
       while(currentState != 0){
         path->addNode(currentState);
+        stateStack.erase(currIt);
         currentState = currentState->father;
       }
 
@@ -107,16 +113,20 @@ Path* SearchAlgorithms::DFS(State *state){
       freeList(stateStack);
       return path;
     }
-
+    stateStack.pop_front();
+    closedStates.push_back(currentState);
+    std::cout<<"pregenerate\n";
     currentState->GenerateChildrens();
     childrens = currentState->childrens;
-
+    std::cout<<"haha\n";
     removeEquals(childrens, stateStack);
     removeEquals(childrens, closedStates);
+    std::cout<<"pos_Remove\n";
     iterator a = stateStack.begin();
     stateStack.splice(a, childrens);
-    }
+    std::cout<<"saindo\n";
   }
+  std::cout<<"saiu";
 
   freeList(closedStates);
   return 0;
